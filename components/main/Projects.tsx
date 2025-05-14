276f77331a129e8a3255f76bedf700056a7b07fc
dihaxn <ihanlaknuka@gmail.com>
2025-05-14 19:48:59 +0530

    new add

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -14,7 +14,7 @@ const Projects = () => {
                     <ProjectCard
                         src="/NextWebsite.png"
                         title="Little Lanka Outlet Management System – Backend User Service"
-                        description="Backend service built with Spring Boot 🔧, using JWT for secure login 🔐, Swagger for easy API docs 📚, and MySQL for real-time inventory & sales tracking 📊."
+                        description="Backend service built with Spring Boot 🔧, using JWT for secure login 🔐, Swagger for easy API docs 📚, and MySQL as Data Base📊."
                         technologies={[
                             "Spring Boot",
                             "Spring Security (JWT)",
